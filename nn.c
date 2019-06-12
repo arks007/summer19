fce83699808f3753743e2c1c6fbec2107c16389a
Sujoy P <sujoy2900@gmail.com>
2019-06-12 18:25:46 -0400

    first implementation of the back propagation algorithm

--- nn.c
+++ nn.c
@@ -38,6 +38,12 @@ float sigmoid(float x){
     return 1/(1 + exp(-x));
 }
 
+/* Returns the derivative of the sigmoid function at a point x */
+/* Assumes that x is the output of the normal sigmoid function */
+float dSigmoid(float x){
+    return x * (1 - x);
+}
+
 /* A function that normalizes the numerics that will be utilized by the neural network */
 float normalize(float x, float xMin, float xMax){
     return (x - xMin)/(xMax - xMin);
@@ -55,8 +61,8 @@ double randDouble2(double x, double y) {
 void weightInitialization(float* iterator, int numNeuronsPerLayer){
     //printf("Weights: ");
     for(int i = 0; i < numNeuronsPerLayer; i++){
-        //*iterator = randDouble2(-1, 1); //initialize the weight b/w -1 and 1
-        *iterator = .1;
+        *iterator = randDouble2(-1, 1); //initialize the weight b/w -1 and 1
+        //*iterator = .1;
         //printf("%f ", *iterator);
         iterator++;
     }
@@ -146,7 +152,7 @@ void modelInfo(neuralNet* model){
     Neuron* neuronPtr = layerPointer -> layerPtr;
     printf("Input layer details:\n");
     for(int i = 0; i < model -> numInputNeurons; i++){
-        printf("Input Neuron %d: value: %5.2f | ", i, neuronPtr -> value);
+        printf("Input Neuron %d: value: %7.4f | ", i, neuronPtr -> value);
         printWeights(neuronPtr -> weightsPtr, model -> numNeuronsPerHiddenLayer);
         neuronPtr++;
     }
@@ -159,11 +165,11 @@ void modelInfo(neuralNet* model){
         neuronPtr = layerPointer -> layerPtr;
         neuralLayer* nextLayerPtr = layerPointer -> nextLayer;
         for(int j = 0; j < layerPointer -> numNeurons; j++){
-            printf("Hidden Neuron %d: value: %5.2f | ", j, neuronPtr -> value);
+            printf("Hidden Neuron %d: value: %7.4f | ", j, neuronPtr -> value);
             printWeights(neuronPtr -> weightsPtr, (nextLayerPtr -> numNeurons));
             neuronPtr++;
         }
-        printf("Layer bias: %5.2f\n", layerPointer -> bias);
+        printf("Layer bias: %7.4f\n", layerPointer -> bias);
         printf("\n");
         layerPointer = layerPointer -> nextLayer;
     }
@@ -172,10 +178,10 @@ void modelInfo(neuralNet* model){
     neuronPtr = layerPointer -> layerPtr;
     for(int i = 0; i < layerPointer -> numNeurons; i++){
         printf("Output layer details:\n");
-        printf("Output neuron %d value: %5.2f\n", i, neuronPtr -> value);
+        printf("Output neuron %d value: %7.4f\n", i, neuronPtr -> value);
         neuronPtr++;
     }
-    printf("Layer bias: %5.2f\n", layerPointer -> bias);
+    printf("Layer bias: %7.4f\n", layerPointer -> bias);
     printf("\n");
 }
 
@@ -184,7 +190,7 @@ void feedForwardHelper(neuralLayer* currentLayer){
     Neuron* currentNeuron = currentLayer -> layerPtr;
     Neuron* nextLayerNeuron = currentLayer -> nextLayer -> layerPtr;
     for(int i = 0; i < currentLayer -> numNeurons; i++){
-        float* weightPtr = currentNeuron -> weightsPtr;
+        float* weightPtr = currentNeuron[i].weightsPtr;
         for(int j = 0; j < currentLayer -> nextLayer -> numNeurons; j++){
             nextLayerNeuron[j].value += weightPtr[j] * currentNeuron[i].value;
         }
@@ -221,36 +227,167 @@ int readInputValues(char arr[], neuralNet* model){
     return i;
 }
 
+float* errorArray(neuralNet* model, float* testValues){
+    float* errors = malloc(sizeof(float) * model -> numOutputNeurons + 1);
+    neuralLayer* layerPtr = model -> inputLayer;
+    for(int i = 0; i < model -> numHiddenLayers + 1; i++){
+        layerPtr = layerPtr -> nextLayer; 
+    }
+    Neuron* NeuronPtr = layerPtr -> layerPtr;
+    float totalError = 0;
+    for(int i = 0; i < model -> numOutputNeurons; i++){
+        totalError += .5 * pow(NeuronPtr[i].value - testValues[i], 2);
+        errors[i] = .5 * pow(NeuronPtr[i].value - testValues[i], 2); 
+    }
+    errors[model -> numOutputNeurons] = totalError;
+    return errors;
+}
 
+float* loadOutputVals(neuralNet* model){
+    float* outputs = malloc(sizeof(float) * model -> numOutputNeurons);
+    neuralLayer* layerPtr = model -> inputLayer;
+    for(int i = 0; i < model -> numHiddenLayers + 1; i++){
+        layerPtr = layerPtr -> nextLayer; 
+    }
+    Neuron* NeuronPtr = layerPtr -> layerPtr;
+    for(int i = 0; i < model -> numOutputNeurons; i++){
+        outputs[i] =  NeuronPtr[i].value;
+    }
+    return outputs;
+}
+
+float backPropagationHelper(float* weightsPtr, neuralLayer* layerPointer, int index, float* outputs, float* desiredVals){
+    float val = 0;
+    if(layerPointer -> nextLayer == NULL){
+        Neuron* outputNeurons = layerPointer -> layerPtr;
+        val = (outputs[index] - desiredVals[index]) * dSigmoid(outputNeurons[index].value) * weightsPtr[index];
+        return val;
+    }
+    else{
+        for(int i = 0; i < layerPointer -> numNeurons; i++){
+        Neuron* destNeurons = layerPointer -> nextLayer -> layerPtr;
+        Neuron* sourceNeurons = layerPointer ->  layerPtr;
+        //val += backPropagationHelper(sourceNeurons -> weightsPtr, layerPointer -> nextLayer, i, outputs, desiredVals) * dSigmoid(destNeurons[i].value) * weightsPtr[i];
+        val += backPropagationHelper(sourceNeurons[index].weightsPtr, layerPointer -> nextLayer, i, outputs, desiredVals);
+        }
+    }
+    
+    return val; 
+}
+
+float dWCalc(neuralNet* model, Neuron sourceNeuron, Neuron destNeuron, neuralLayer* destLayer, int layerIndex, int subIndex, float* outputs, float* desiredVals){
+    float alpha = 0.01;
+    float deltaW = 0;
+    //special case if a weight is between a hidden neuron and an output neuron 
+    //do not perform recursive backpropagation 
+    if(layerIndex == model -> numHiddenLayers){
+        deltaW = alpha * ((destNeuron.value) - desiredVals[subIndex]) * dSigmoid(destNeuron.value) * sourceNeuron.value;
+    }
+    //the destination neuron is in a hidden layer so recursive back propagation must be applied to traverse through 
+    else
+    {
+        deltaW = alpha * backPropagationHelper(sourceNeuron.weightsPtr, destLayer, subIndex, outputs, desiredVals) * dSigmoid(destNeuron.value) * sourceNeuron.value;
+    }
+    return deltaW;
+}
+
+/* the main driver for the backPropagation algorithm */
+void backPropagation(neuralNet* model, float* desiredVals){
+    // loop through the input and all hidden layers
+    neuralLayer* layerPointer = model -> inputLayer;
+    float* outputs = loadOutputVals(model);
+    for(int i = 0; i < model -> numHiddenLayers + 1; i++){
+        //loop through all of the neurons in a particular layer
+        Neuron* sourceNeuronPointer = layerPointer -> layerPtr;
+        Neuron* destNeuronPointer = layerPointer -> nextLayer -> layerPtr;
+        for(int j = 0; j < layerPointer -> numNeurons; j++){
+            //loop through all of the weights in a Neuron's weightPtr
+            float* neuronWeightPtr = sourceNeuronPointer -> weightsPtr;
+            for(int k = 0; k < layerPointer -> nextLayer -> numNeurons; k++){
+                //passing on the next layer because it might be the output layer
+                float dW = dWCalc(model, sourceNeuronPointer[j], destNeuronPointer[k], layerPointer -> nextLayer, i, k, outputs, desiredVals);
+                neuronWeightPtr[k] -= dW;
+            }
+        }
+        layerPointer = layerPointer -> nextLayer;
+    }
+}
 
 int main(int argc, char* argv[]){
-    /* initialize the rand functions */
-    srand((long)time(NULL));
     
-    /* Generate a model */
-    double timeElapsed = 0.0;
-    clock_t start = clock();
-    neuralNet* testModel = generateNNModel(atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
-    clock_t end = clock();
-    
-    /* Load input values for the neural network */
-    int numValsRead = readInputValues(argv[1], testModel); 
-    if(numValsRead != testModel -> numInputNeurons){
-        printf("!!!WARNING: ERROR IN READING IN VALUES!!!");
-    }
 
-    /*run feed forward*/
+    //creating the input neurons
+    Neuron* inputNeurons = malloc(sizeof(Neuron) * 3); 
+    inputNeurons[0].value = 1;
+    inputNeurons[1].value = 4;
+    inputNeurons[2].value = 5;
+    float* inputWeight1 = malloc(sizeof(float) * 2);
+    inputWeight1[0] = .1;
+    inputWeight1[1] = .2;
+    inputNeurons[0].weightsPtr = inputWeight1;
+    float* inputWeight2 = malloc(sizeof(float) * 2);
+    inputWeight2[0] = .3;
+    inputWeight2[1] = .4;
+    inputNeurons[1].weightsPtr = inputWeight2;
+    float* inputWeight3 = malloc(sizeof(float) * 2);
+    inputWeight3[0] = .5;
+    inputWeight3[1] = .6;
+    inputNeurons[2].weightsPtr = inputWeight3;
+    neuralLayer* inputLayer = malloc(sizeof(neuralLayer));
+    inputLayer -> layerPtr = inputNeurons;
+    inputLayer -> prevLayer = NULL;
+    inputLayer -> nextLayer = NULL;
+    inputLayer -> numNeurons = 3;
+
+    Neuron* hiddenNeurons = malloc(sizeof(Neuron) * 2);
+    hiddenNeurons[0].value = 0;
+    hiddenNeurons[1].value = 0;
+    float* hiddenWeight1 = malloc(sizeof(float) * 2);
+    hiddenWeight1[0] = .7;
+    hiddenWeight1[1] = .8;;
+    hiddenNeurons[0].weightsPtr = hiddenWeight1;
+    float* hiddenWeight2 = malloc(sizeof(float) * 2);
+    hiddenWeight2[0] = .9;
+    hiddenWeight2[1] = .1;;
+    hiddenNeurons[1].weightsPtr = hiddenWeight2;
+    neuralLayer* hiddenLayer = malloc(sizeof(neuralLayer));
+    hiddenLayer -> layerPtr = hiddenNeurons;
+    hiddenLayer -> nextLayer = NULL;
+    hiddenLayer -> prevLayer = inputLayer;
+    hiddenLayer -> numNeurons = 2;
+    inputLayer -> nextLayer = hiddenLayer;
+    hiddenLayer -> bias = .5;
+
+    Neuron* outputNeurons = malloc(sizeof(Neuron) * 2);
+    outputNeurons[0].value = 0;
+    outputNeurons[1].value = 0;
+    neuralLayer* outputLayer = malloc(sizeof(neuralLayer));
+    outputLayer -> numNeurons = 2;
+    outputLayer -> bias = .5;
+    outputLayer -> layerPtr = outputNeurons;
+    outputLayer -> prevLayer = hiddenLayer;
+    hiddenLayer -> nextLayer = outputLayer;
+    outputLayer -> nextLayer = NULL;
+
+
+    neuralNet* testModel = malloc(sizeof(neuralNet));
+    testModel -> inputLayer = inputLayer;
+    testModel -> numHiddenLayers = 1;
+    testModel -> numNeuronsPerHiddenLayer = 2;
+    testModel -> numInputNeurons = 3;
+    testModel -> numOutputNeurons = 2;
+
+    modelInfo(testModel);
     feedForward(testModel);
+    modelInfo(testModel);
     
-    timeElapsed += (double)(end - start)/CLOCKS_PER_SEC;
-    printf("Time elapsed is %f seconds \n", timeElapsed);
+    float* desiredVals = malloc(sizeof(float) * 2);
+    desiredVals[0] = .1;
+    desiredVals[1] = .05;
+    backPropagation(testModel, desiredVals);
+    modelInfo(testModel);
 
-    /*check changes*/
-    if(atoi(argv[6]) == 1){
-        modelInfo(testModel);
-    }
-
-       
     return 0;
 }
 
+ 
