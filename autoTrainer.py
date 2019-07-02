47fd6482444c12a84118d383fcaf217dfd498d8a
Sujoy P <sujoy2900@gmail.com>
2019-07-02 01:30:14 -0400

    auto train can identify wgm shift vals whose prediction exceeds a desired error %

--- autoTrainer.py
+++ autoTrainer.py
@@ -25,29 +25,50 @@ print("Loaded model from disk")
 xWindow = .008
 center = 1300
 xRes = 500
-numShifts = 60
+numShifts = 120
 
 #dealing with positive shifts only 
 xStart = center
 xStop = center - xWindow
-xStep = .0001
+xStep = .00005
 
 #create a 2D-list of difference distribution lists
 data = genWGMShiftData(1, numShifts, xStep, 15, xRes, 0.001)
 #data = genWGMShiftData(1, 60, .0001, 15, 500, 0.001)
 diffDistsArr = data[0]
+shiftArr = data[1]
 
-#error threshold of 5%
-threshold = 5
+#error threshold (in %)
+threshold = 10
 #a list that keeps track of faulty wgm shift distances (error exceeds a certain percent threshold)
 errShiftArr = []
 
 sc = StandardScaler()
 diffDistsArr = sc.fit_transform(diffDistsArr)
 
+
 wgmShiftPreds = loaded_model.predict(diffDistsArr)
 #print(wgmShiftPreds)
-for element in 
+for index, element in enumerate(wgmShiftPreds, 0):
+    #prevent divide by 0 errors 
+    if(shiftArr[index] == 0):
+        #shift value should be as close to 0 as possible 
+        if(element[0] > 0.000001):
+            if shiftArr[index] not in errShiftArr:
+                errShiftArr.append(shiftArr[index])
+
+    elif(abs(100*(element[0] - shiftArr[index])/shiftArr[index]) > threshold):
+        if shiftArr[index] not in errShiftArr:
+            errShiftArr.append(shiftArr[index])
+
+#errShiftArr now contains wgm shift values that the model predicts an unacceptable value for 
+#further train with these values at varying q factor and amp values 
+
+
+
+print(len(shiftArr))
+print(errShiftArr)
+
 
 
 
